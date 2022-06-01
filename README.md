62c99f4a9f3ac2b2ecfadc2e1cd61a749af3ef35
Cedric Spindler <cedric.spindler@fhnw.ch>
2022-06-01 17:32:52 +0200

    Add repo boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mitwelten DB Backend
+
 Work in progress. Questions? Contact [mitwelten.technik@fhnw.ch](mailto:mitwelten.technik@fhnw.ch)
---
+++ credentials_example.py
@@ -0,0 +1,8 @@
+class DbConfig(object):
+  host = 'localhost'
+  port = 5432
+  database = 'db'
+  user = 'postgres'
+  password = 'secret'
+
+db = DbConfig()
