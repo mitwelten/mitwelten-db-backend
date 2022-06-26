9a7b6892613137d44b6a719adab6db6e6e54dfbc
Cedric Spindler <cedric.spindler@fhnw.ch>
2022-06-26 02:16:59 +0200

    Implement add/update/validate deployments

--- services/results/main.py
+++ services/results/main.py
@@ -1,18 +1,19 @@
 import sys
 from typing import List
-
 import databases
 
 import sqlalchemy
-from sqlalchemy.sql import select, func, and_, desc
+from sqlalchemy.sql import select, func, and_, desc, text, LABEL_STYLE_TABLENAME_PLUS_COL
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from asyncpg.exceptions import ExclusionViolationError
 
 from tables import nodes, locations, deployments, results, tasks, species, species_day
-from models import Deployment, Result, Species
+from models import Deployment, Result, Species, DeploymentResponse, DeploymentRequest, Node
 
 sys.path.append('../../')
 import credentials as crd
@@ -32,6 +33,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    print(f"{request}: {exc_str}")
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 @app.on_event('startup')
 async def startup():
@@ -109,9 +117,38 @@ async def read_input():
 async def read_nodes():
     return await database.fetch_all(select(nodes))
 
-@app.get('/deployments', response_model=List[Deployment])
-async def read_nodes():
-    return await database.fetch_all(select(deployments).select_from(deployments.outerjoin(nodes).outerjoin(locations)))
+@app.get('/node/{id}', response_model=Node)
+async def read_node(id: int) -> Node:
+    return await database.fetch_one(select(nodes).where(nodes.c.node_id == id))
+
+@app.get('/deployments', response_model=List[DeploymentResponse])
+async def read_deployments() -> List[DeploymentResponse]:
+
+    query = select(deployments.alias('d').outerjoin(nodes.alias('n')).outerjoin(locations.alias('l'))).\
+        set_label_style(LABEL_STYLE_TABLENAME_PLUS_COL)
+    result = await database.fetch_all(query)
+    response = []
+    for r in result:
+        d = { c: r['d_'+c] for c in deployments.columns.keys() }
+        d['location'] = { c: r['l_'+c] for c in locations.columns.keys() }
+        d['node'] = { c: r['n_'+c] for c in nodes.columns.keys() }
+        response.append(d)
+    return response
+
+@app.get('/deployment/{id}', response_model=DeploymentResponse)
+async def read_deployment(id: int) -> DeploymentResponse:
+
+    query = select(deployments.alias('d').outerjoin(nodes.alias('n')).outerjoin(locations.alias('l'))).\
+        where(text('deployment_id = :id').bindparams(id=id)).set_label_style(LABEL_STYLE_TABLENAME_PLUS_COL)
+
+    r = await database.fetch_one(query)
+    if r == None:
+        raise HTTPException(status_code=404, detail='Deployment not found')
+
+    d = { c: r['d_'+c] for c in deployments.columns.keys() }
+    d['location'] = { c: r['l_'+c] for c in locations.columns.keys() }
+    d['node'] = { c: r['n_'+c] for c in nodes.columns.keys() }
+    return d
 
 @app.post('/deployments', response_model=None)
 async def add_node(body: Deployment) -> None:
@@ -123,6 +160,129 @@ async def add_node(body: Deployment) -> None:
         }))
     except ExclusionViolationError as e:
         raise HTTPException(status_code=409, detail=str(e))
+
+@app.put('/deployments', response_model=None)
+async def update_node(body: DeploymentRequest) -> None:
+    transaction = await database.transaction()
+    # TODO: put all except the except block into one function to be used in multiple occasions
+    try:
+        # check if the location exists
+        point = f'point({float(body.location.lat)}, {float(body.location.lon)})'
+        thresh_1m = 0.0000115
+        location_query = f'''
+        select location_id from {crd.db.schema}.locations
+        where location <-> {point} < {thresh_1m}
+        order by location <-> {point}
+        limit 1
+        '''
+        result = await database.fetch_one(query=location_query)
+
+        location_id = None
+        if result == None:
+            # insert new location
+            print('adding location...')
+            loc_insert_query = f'''
+            insert into {crd.db.schema}.locations(location, type)
+            values (point({body.location.lat},{body.location.lon}), 'user-added')
+            returning location_id
+            '''
+            location_id = await database.execute(loc_insert_query)
+        else:
+            print('using existing location...')
+            location_id = result.location_id
+
+        if hasattr(body, 'deployment_id') and body.deployment_id != None:
+            # this is an update
+            # update the record to see if it conflicts
+            print('updating...')
+            pprint(dict(body))
+            await database.execute(deployments.update().\
+                where(deployments.c.deployment_id == body.deployment_id).\
+                values({
+                    deployments.c.node_id: body.node_id,
+                    deployments.c.location_id: location_id,
+                    deployments.c.period: body.period,
+                }
+            ))
+        else:
+            # this is a new record, try to insert
+            print('inserting...')
+            await database.execute(deployments.insert().\
+                values({
+                    deployments.c.node_id: body.node_id,
+                    deployments.c.location_id: location_id,
+                    deployments.c.period: body.period,
+                }
+            ))
+    except ExclusionViolationError as e:
+        await transaction.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        await transaction.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
+    else:
+        print('inserted/updated, no overlap')
+        await transaction.commit()
+
+@app.put('/validate/deployment')
+async def validate_node(body: DeploymentRequest) -> None:
+    transaction = await database.transaction()
+    try:
+        # check if the location exists
+        point = f'point({float(body.location.lat)}, {float(body.location.lon)})'
+        thresh_1m = 0.0000115
+        location_query = f'''
+        select location_id from {crd.db.schema}.locations
+        where location <-> {point} < {thresh_1m}
+        order by location <-> {point}
+        limit 1
+        '''
+        result = await database.fetch_one(query=location_query)
+
+        location_id = None
+        if result == None:
+            # insert new location
+            loc_insert_query = f'''
+            insert into {crd.db.schema}.locations(location, type)
+            values (point({body.location.lat},{body.location.lon}), 'user-added')
+            returning location_id
+            '''
+            location_id = await database.execute(loc_insert_query)
+        else:
+            location_id = result.location_id
+
+        if hasattr(body, 'deployment_id') and body.deployment_id != None:
+            # this is an update
+            # update the record to see if it conflicts
+            await database.execute(deployments.update().\
+                where(deployments.c.deployment_id == body.deployment_id).\
+                values({
+                    deployments.c.node_id: body.node_id,
+                    deployments.c.location_id: location_id,
+                    deployments.c.period: body.period,
+                }
+            ))
+        else:
+            # this is a new record, try to insert
+            await database.execute(deployments.insert().\
+                values({
+                    deployments.c.node_id: body.node_id,
+                    deployments.c.location_id: location_id,
+                    deployments.c.period: body.period,
+                }
+            ))
+    except ExclusionViolationError as e:
+        print('validator', e)
+        await transaction.rollback()
+        return True
+    except Exception as e:
+        print('validator', e)
+        await transaction.rollback()
+        return True
+    else:
+        print('validator: no overlap')
+        await transaction.rollback()
+        return False
 
 @app.get('/')
 async def root():
--- services/results/models.py
+++ services/results/models.py
@@ -1,10 +1,14 @@
 from typing import Optional
 from datetime import datetime
 from sqlalchemy.dialects.postgresql import TSTZRANGE
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, constr
 from asyncpg.types import Range
 
 class TimeStampRange(TSTZRANGE):
+
+    def stripz(ts):
+        assert isinstance(ts, str)
+        return ts[0:len(ts)-1] if ts[-1] == 'Z' else ts
 
     @classmethod
     def __get_validators__(cls):
@@ -17,10 +21,10 @@ class TimeStampRange(TSTZRANGE):
         elif 'start' in val and 'end' in val:
             lower = None
             upper = None
-            if val['start'] != '':
-                lower = datetime.fromisoformat(val['start'])
-            if val['end'] != '':
-                upper = datetime.fromisoformat(val['end'])
+            if val['start'] != '' and val['start'] != None:
+                lower = datetime.fromisoformat(cls.stripz(val['start']))
+            if val['end'] != '' and val['end'] != None:
+                upper = datetime.fromisoformat(cls.stripz(val['end']))
             return Range(lower=lower, upper=upper)
 
 class Result(BaseModel):
@@ -36,6 +40,40 @@ class Species(BaseModel):
     confidence: float
     time_start: datetime
 
+class Point(BaseModel):
+    '''
+    Coordinate in WGS84 format
+    '''
+    lat: float = Field(..., example=47.53484943172696, title="Latitude (WGS84)")
+    lon: float = Field(..., example=7.612519197679952, title="Longitude (WGS84)")
+
+class Location(BaseModel):
+    '''
+    A location record, describing metadata of a coordinate
+    '''
+    id: Optional[int] = None
+    location: Point
+    type: Optional[str] = None
+    name: Optional[str] = None
+    description: Optional[str] = None
+
+class Node(BaseModel):
+    '''
+    A device deployed in the field, commondly collecting and/or processing data
+    '''
+    node_id: Optional[int] = None
+    node_label: constr(regex=r'\d{4}-\d{4}')
+    type: str
+    serial_number: Optional[str]
+    description: Optional[str]
+    platform: Optional[str]
+    connectivity: Optional[str]
+    power: Optional[str]
+    hardware_version: Optional[str]
+    software_version: Optional[str]
+    firmware_version: Optional[str]
+
+
 class Deployment(BaseModel):
     '''
     Deployment
@@ -44,4 +82,22 @@ class Deployment(BaseModel):
     deployment_id: Optional[int] = None
     node_id: int
     location_id: int
+    period: TimeStampRange
+
+class DeploymentResponse(Deployment):
+    '''
+    DeploymentResponse
+    '''
+
+    location: Optional[Location]
+    node: Optional[Node]
+
+class DeploymentRequest(BaseModel):
+    '''
+    Deployment
+    '''
+
+    deployment_id: Optional[int] = None
+    node_id: int
+    location: Point
     period: TimeStampRange
