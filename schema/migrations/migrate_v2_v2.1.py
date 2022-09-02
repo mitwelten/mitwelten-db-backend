9721bbb40d1da812c5276fc382f62be838437647
Cedric Spindler <cedric.spindler@fhnw.ch>
2022-09-02 08:01:02 +0200

    Add progress output

--- schema/migrations/migrate_v2_v2.1.py
+++ schema/migrations/migrate_v2_v2.1.py
@@ -3,6 +3,7 @@ import json
 import psycopg2 as pg
 from psycopg2.extras import DictCursor, execute_values
 from psycopg2.extensions import AsIs
+from tqdm import tqdm
 
 sys.path.append('../../')
 import credentials as crd
@@ -12,10 +13,11 @@ cursor = connection.cursor(cursor_factory=DictCursor)
 
 
 # insert / migrate node tags
+print('migrating dev.tags')
 cursor.execute('select * from dev.tags')
 tags_records_dev = cursor.fetchall()
 tags_idmap = {} # tags_idmap dev -> prod
-for trd in tags_records_dev:
+for trd in tqdm(tags_records_dev):
     id_dev = trd['tag_id']
     cols = list(trd.keys())[1:] # drop id
     vals = tuple(trd[c] for c in cols)
@@ -24,10 +26,11 @@ for trd in tags_records_dev:
 
 
 # migrate nodes, create id-mapping
+print('migrating dev.nodes')
 cursor.execute('select * from dev.nodes')
 nodes_records_dev = cursor.fetchall()
 nodes_idmap = {} # nodes_idmap dev -> prod
-for nrd in nodes_records_dev:
+for nrd in tqdm(nodes_records_dev):
     id_dev = nrd['node_id']
     cols = list(nrd.keys())[1:] # drop id
     vals = tuple(nrd[c] for c in cols)
@@ -36,16 +39,18 @@ for nrd in nodes_records_dev:
 
 
 # locations
+print('creating id-mapping for dev.locations')
 cursor.execute('select * from dev.locations')
 locations_records_dev = cursor.fetchall()
 locations_dev = {r['location_id']:r for r in locations_records_dev}
 
 
 # deployments
+print('migrating dev.deployments, integrating dev.locations into prod.deployments')
 cursor.execute('select * from dev.deployments')
 deployments_records_dev = cursor.fetchall()
 deployments_idmap = {} # deployments_idmap dev -> prod
-for drd in deployments_records_dev:
+for drd in tqdm(deployments_records_dev):
     id_dev = drd['deployment_id']
 
     node_id_prod = nodes_idmap[drd['node_id']]
@@ -77,9 +82,10 @@ for drd in deployments_records_dev:
 # migrate node - tag assignment to deployment - tag assignment,
 # tagging the newer deployment if multiple match,
 # only cam nodes for FS2 had been tagged so far
+print('migrating dev.mm_tags_nodes to prod.mm_tags_deployments')
 cursor.execute('select * from dev.mm_tags_nodes')
 mm_tags_nodes_records_dev = cursor.fetchall()
-for mmtdr in mm_tags_nodes_records_dev:
+for mmtdr in tqdm(mm_tags_nodes_records_dev):
     tag_id_prod = tags_idmap[mmtdr['tags_tag_id']]
     node_id_prod = nodes_idmap[mmtdr['nodes_node_id']]
     # deployments_deployment_id <- nodes_node_id
@@ -91,10 +97,11 @@ for mmtdr in mm_tags_nodes_records_dev:
 
 
 # entries
+print('migrating dev.entries, integrating dev.locations into prod.entries')
 cursor.execute('select * from dev.entries')
 entries_records_dev = cursor.fetchall()
 entries_idmap = {}
-for erd in entries_records_dev:
+for erd in tqdm(entries_records_dev):
     id_dev = erd['entry_id']
     cols = list(erd.keys())[1:] # drop id
     vals = list(erd[c] for c in cols)
@@ -107,10 +114,11 @@ for erd in entries_records_dev:
 
 
 # files_entry
+print('migrating dev.files_entry')
 cursor.execute('select * from dev.files_entry')
 files_entry_records_dev = cursor.fetchall()
 files_entry_idmap = {}
-for ferd in files_entry_records_dev:
+for ferd in tqdm(files_entry_records_dev):
     id_dev = ferd['file_id']
     cols = list(ferd.keys())[1:] # drop id
     vals = list(ferd[c] for c in cols)
@@ -121,9 +129,10 @@ for ferd in files_entry_records_dev:
 
 
 # migrate entry - tag assignment
+print('migrating dev.mm_tags_entries')
 cursor.execute('select * from dev.mm_tags_entries')
 mm_tags_entries_records_dev = cursor.fetchall()
-for mmetrd in mm_tags_entries_records_dev:
+for mmetrd in tqdm(mm_tags_entries_records_dev):
     tag_id_prod = tags_idmap[mmetrd['tags_tag_id']]
     entry_id_prod = entries_idmap[mmetrd['entries_entry_id']]
     insert_stmt = 'insert into prod.mm_tags_entries (tags_tag_id, entries_entry_id) values (%s, %s)'
@@ -132,6 +141,7 @@ for mmetrd in mm_tags_entries_records_dev:
 
 # birdnet_configs
 # using a better method with `execute_values`
+print('migrating dev.birdnet_configs')
 cursor.execute('select * from dev.birdnet_configs')
 configs_records_dev = cursor.fetchall()
 if len(configs_records_dev) > 0:
@@ -148,12 +158,16 @@ if len(configs_records_dev) > 0:
 
 
 # birdnet_species_occurrence
+print('copying dev.birdnet_species_occurrence')
 cursor.execute('insert into prod.birdnet_species_occurrence (select * from dev.birdnet_species_occurrence)')
 
 
 # files_audio
 # - set deployment id from 'select deployment_id from prod.deployments where node_id = %s and files_audio.time <@ period
 # use server side cursor for tables with many records
+print('migrating dev.files_audio, with related records of dev.birdnet_tasks and dev.birdnet_results')
+cursor.execute('select count(file_id) from dev.files_audio')
+pbar = tqdm(total=cursor.fetchone()[0])
 cursor_exp = connection.cursor(cursor_factory=DictCursor, name='export_dev')
 cursor_exp.execute('select * from dev.files_audio')
 while True:
@@ -192,9 +206,11 @@ while True:
                 results_prod.append(tuple([task_id_prod, file_id_prod, *result]))
             # insert results (-> task_id, file_id)
             execute_values(cursor, 'insert into prod.birdnet_results (task_id, file_id, time_start, time_end, confidence, species) values %s', results_prod)
-
+        pbar.update(1)
+pbar.close()
 
 # # files_image (deployments fs1 pending)
+# print('migrating dev.files_image')
 # cursor_exp.execute('''
 # insert into prod.files_image(object_name, sha256, time, deployment_id, file_size, resolution, created_at, updated_at)
 # (select object_name, sha256, time,
@@ -204,7 +220,9 @@ while True:
 
 
 # sensordata_env (deployments fs1 pending)
+# print('migrating dev.sensordata_env')
 # sensordata_pax (deployments fs1 pending)
+# print('migrating dev.sensordata_pax')
 
 
 # connection.commit()
