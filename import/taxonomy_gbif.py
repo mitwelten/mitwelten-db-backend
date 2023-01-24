9b76144728f405f8c2034ec712afd37bd5eb233c
Cedric Spindler <cedric.spindler@fhnw.ch>
2023-01-24 15:01:55 +0100

    Implement import of pollinator/flower taxons

--- import/taxonomy_gbif.py
+++ import/taxonomy_gbif.py
@@ -8,16 +8,30 @@ sys.path.append('../')
 import credentials as crd
 
 keyMap = [ # map db fieldnames to keys in GBIF response
+    # for one species there may exist subspecies in GBIF,
+    # referred to by a usage key, which is used here as identifier in 'species_id'
+    {'db': 'species_id', 'gbif': 'usageKey'   , 'gbif_label': 'scientificName', 'rank': 'SUBSPECIES'},
     # for one speciesKey there may exist synonyms in GBIF,
     # prefer the name that matched with the lookup (canonicalName)
-    {'db': 'species_id', 'gbif': 'speciesKey' , 'gbif_label': 'canonicalName'},
-    {'db': 'genus_id',   'gbif': 'genusKey'   , 'gbif_label': 'genus'},
-    {'db': 'family_id',  'gbif': 'familyKey'  , 'gbif_label': 'family'},
-    {'db': 'class_id',   'gbif': 'classKey'   , 'gbif_label': 'class'},
-    {'db': 'phylum_id',  'gbif': 'phylumKey'  , 'gbif_label': 'phylum'},
-    {'db': 'kingdom_id', 'gbif': 'kingdomKey' , 'gbif_label': 'kingdom'}
+    {'db': 'species_id', 'gbif': 'speciesKey' , 'gbif_label': 'canonicalName',  'rank': 'SPECIES'},
+    {'db': 'genus_id',   'gbif': 'genusKey'   , 'gbif_label': 'genus',          'rank': 'GENUS'},
+    {'db': 'family_id',  'gbif': 'familyKey'  , 'gbif_label': 'family',         'rank': 'FAMILY'},
+    {'db': 'class_id',   'gbif': 'classKey'   , 'gbif_label': 'class',          'rank': 'CLASS'},
+    {'db': 'phylum_id',  'gbif': 'phylumKey'  , 'gbif_label': 'phylum',         'rank': 'PHYLUM'},
+    {'db': 'kingdom_id', 'gbif': 'kingdomKey' , 'gbif_label': 'kingdom',        'rank': 'KINGDOM'}
 ]
-dbfields = ','.join([x['db'] for x in keyMap]) # field definition for sql insert
+dbfields = ','.join([x['db'] for x in keyMap[1:]]) # field definition for sql insert
+
+pollinator_taxons = [ # manual selection of pollinator / flower taxons
+    'Apis',
+    'Andrenidae',
+    'Bombus',
+    'Syrphidae',
+    'Muscidae',
+    'Leucanthemum',
+    'Daucus carota carota',
+    'Centaurea'
+]
 
 def fetch_labels(key):
     labels = {}
@@ -52,10 +66,14 @@ def main():
     print('fetching taxonomy for all birdnet inferred species in db')
     species_data = [ species.name_backbone(x) for x in splist]
 
+    print('fetching taxonomy for pollinator data')
+    species_data.extend([ species.name_backbone(x) for x in pollinator_taxons])
+
     for taxon in species_data:
         print('parsing', taxon['canonicalName'])
+        entrypoint = next((i for (i, d) in enumerate(keyMap) if d['rank'] == taxon['rank']), 0)
         # check if label record exist in db, query and insert if not
-        for k in keyMap:
+        for k in keyMap[entrypoint:]:
             cursor.execute('select * from prod.taxonomy_labels where label_id = %s', (taxon[k['gbif']],))
             label = cursor.fetchall()
             if not len(label):
@@ -65,8 +83,15 @@ def main():
                 cursor.execute('insert into prod.taxonomy_labels (label_id, label_sci, label_de, label_en) values (%s,%s,%s,%s)',
                     (taxon[k['gbif']], taxon[k['gbif_label']], labels.get('de'), labels.get('en')))
             else: break # if the label exists, the parents exist, too
+        # account for null values if higher order taxon
+        values = []
+        for i, k in enumerate(keyMap[1:]):
+            if i < (entrypoint-1): values.append(None)
+            else: values.append(taxon[k['gbif']])
+        # put usage key into species_id when present (== subspecies)
+        if taxon['rank'] == 'SUBSPECIES': values[0] = taxon[keyMap[0]['gbif']]
         cursor.execute(f'insert into prod.taxonomy_species ({dbfields}) values (%s,%s,%s,%s,%s,%s) on conflict do nothing',
-            tuple((taxon[k] for k in [x['gbif'] for x in keyMap])))
+            tuple(values))
         connection.commit()
 
 if __name__ == '__main__':
