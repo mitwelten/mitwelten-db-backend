5e04d3e455ac2a50a7c3a3b5aab4d19b9a739be0
Cedric Spindler <cedric.spindler@fhnw.ch>
2023-03-17 15:08:37 +0100

    Extend schema for meteodata

--- schema/mitwelten_cache_v1.sql
+++ schema/mitwelten_cache_v1.sql
@@ -4,6 +4,8 @@
 
 BEGIN;
 
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
 CREATE TABLE IF NOT EXISTS public.gbif
 (
     "key" bigint,
@@ -35,9 +37,52 @@ CREATE TABLE IF NOT EXISTS public.gbif
     PRIMARY KEY ("key")
 );
 
+CREATE TABLE public.station (
+    station_id TEXT NOT NULL,
+    station_name TEXT NOT NULL,
+    data_src TEXT NOT NULL,
+    location point NOT NULL,
+    altitude integer,
+    PRIMARY KEY (station_id)
+
+);
+
+CREATE TABLE public.parameter (
+    param_id TEXT NOT NULL,
+    unit TEXT NOT NULL,
+    description TEXT,
+    PRIMARY KEY (param_id)
+);
+
+CREATE TABLE public.meteodata (
+    ts timestamptz NOT NULL,
+    param_id TEXT REFERENCES parameter(param_id),
+    station_id TEXT REFERENCES station(station_id),
+    value double precision NOT NULL,
+    UNIQUE (ts, param_id, station_id)
+);
+
+SELECT create_hypertable('meteodata', 'ts');
+
+CREATE INDEX meteodata_station_id_idx
+ON public.meteodata (station_id, ts DESC);
+
+CREATE INDEX meteodata_param_id_idx
+ON public.meteodata (param_id, ts DESC);
+
+
 END;
 
 ALTER TABLE IF EXISTS public.gbif
+    OWNER TO mw_cache_admin;
+
+ALTER TABLE IF EXISTS public.station
+    OWNER TO mw_cache_admin;
+
+ALTER TABLE IF EXISTS public.parameter
+    OWNER TO mw_cache_admin;
+
+ALTER TABLE IF EXISTS public.meteodata
     OWNER TO mw_cache_admin;
 
 GRANT USAGE ON SCHEMA public TO
