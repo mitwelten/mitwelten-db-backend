7749559acc5bff0986cd76ccfca83f0bcf6cf34b
Andri Wild <andri.wild@students.fhnw.ch>
2024-03-05 12:00:45 +0100

    add thumbnail size and formats to api.config

--- services/api/config.py
+++ services/api/config.py
@@ -2,3 +2,13 @@ import sys
 sys.path.append('../')
 
 import credentials as crd
+
+supported_image_formats = {
+    'image/png':  'png',
+    'image/jpg':  'jpg',
+    'image/jpeg': 'jpeg',
+    'image/gif':  'gif',
+    'image/webp': 'webp',
+}
+
+thumbnail_size = (64, 64)
