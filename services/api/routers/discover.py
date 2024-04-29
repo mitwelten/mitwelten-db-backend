4eedfa0fe7741d9029502d3b9af0d2404f8acbdd
Andri Wild <andri.wild@students.fhnw.ch>
2024-04-29 09:17:40 +0200

    add GET `/discover/birds/top` endpoint #65

--- services/api/routers/discover.py
+++ services/api/routers/discover.py
@@ -1,15 +1,16 @@
+import credentials as crd
 from datetime import datetime
-from typing import List, Optional
-
 from fastapi import APIRouter, Query
-from sqlalchemy.sql import desc
+from pandas import to_timedelta
 from sqlalchemy.sql import select, text, func, between
+from typing import Optional
 
 from api.database import database
 from api.models import (
-    HotspotDataPollinatorsResponse, BirdSpeciesCount
+    BirdSpeciesCount,
+    HotspotDataPollinatorsResponse,
 )
-from api.tables import (files_image, mm_tags_deployments, pollinators, image_results, files_audio, birdnet_results)
+from api.tables import (files_image, mm_tags_deployments, pollinators, image_results)
 
 router = APIRouter(tags=['discover'])
 
@@ -43,15 +44,51 @@ async def get_pollinator_hotspots(
     )
 
 
-@router.get('/discover/birds/top3/{deployment_id}', response_model=List[BirdSpeciesCount])
-async def get_top3(deployment_id: int):
-    query = select(birdnet_results.c.species, func.count().label('count')).\
-        select_from(files_audio).\
-        outerjoin(birdnet_results, files_audio.c.file_id == birdnet_results.c.file_id).\
-        where(files_audio.c.deployment_id == deployment_id).\
-        where(birdnet_results.c.confidence > 0.9).\
-        group_by(birdnet_results.c.species).\
-        order_by(desc(func.count())).\
-        limit(3)
+@router.get('/discover/birds/top/{deployment_id}')
+async def get_stacked_bar(
+    deployment_id: int,
+    time_from: Optional[datetime] = Query(None, alias='from', example='2020-06-22T18:00:00.000Z'),
+    time_to: Optional[datetime] = Query(None, alias='to', example='2024-04-22T20:00:00.000Z'),
+    bucket_width: str = "1d",
+    convidence: float = 0.9,
+    limit_per_day: int = 3) -> list[BirdSpeciesCount]:
 
-    return await database.fetch_all(query)
+    time_from_condition = "AND time >= :time_from" if time_from else ""
+    time_to_condition = "AND time <= :time_to" if time_to else ""
+
+    query = text(f"""
+    WITH ranked_results AS (
+        SELECT
+            br.species,
+            time_bucket(:bucket_width, time) AS bucket,
+            count(*),
+            ROW_NUMBER() OVER (PARTITION BY time_bucket(:bucket_width, time) ORDER BY count(*) DESC)
+        FROM
+            {crd.db.schema}.files_audio AS audio
+            LEFT OUTER JOIN {crd.db.schema}.birdnet_results AS br ON audio.file_id = br.file_id
+        WHERE
+            audio.deployment_id = :deployment_id AND br.confidence > :convidence
+            {time_from_condition}
+            {time_to_condition}
+        GROUP BY br.species, bucket
+    )
+    SELECT species, bucket, count
+    FROM ranked_results
+    WHERE row_number <= :limit_per_day
+    ORDER BY bucket, count DESC
+    """).bindparams(
+            bucket_width = to_timedelta(bucket_width).to_pytimedelta(),
+            deployment_id = deployment_id,
+            convidence = convidence,
+            limit_per_day = limit_per_day)
+
+    if time_from:
+        query = query.bindparams(time_from = time_from)
+    if time_to:
+        query = query.bindparams(time_to = time_to)
+
+    result = await database.fetch_all(query=query)
+    return [BirdSpeciesCount(
+        species = entry["species"],
+        count = entry["count"],
+        bucket = entry["bucket"]) for entry in result]
