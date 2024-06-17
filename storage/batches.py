9f18faa0122685138c9e13f4d05ff48bf5567bf9
Cedric Spindler <cedric.spindler@fhnw.ch>
2024-06-17 10:05:17 +0200

    Add schema to batches queries

--- storage/batches.py
+++ storage/batches.py
@@ -13,15 +13,15 @@ batches = [
     -- count 1700849, total size 7170 GB
     with selected_deployments as (
         select d.deployment_id as deployment_id
-        from deployments d
-        left join mm_tags_deployments mm on mm.deployments_deployment_id = d.deployment_id
-        left join tags t on t.tag_id = mm.tags_tag_id
-        left join nodes n on d.node_id = n.node_id
+        from prod.deployments d
+        left join prod.mm_tags_deployments mm on mm.deployments_deployment_id = d.deployment_id
+        left join prod.tags t on t.tag_id = mm.tags_tag_id
+        left join prod.nodes n on d.node_id = n.node_id
         where t.name = 'FS3'
             and n.type = 'Pollinator Cam'
             and d.deployment_id not in (1198, 1203, 1215, 1224, 2150)
     )
-    -- select count(*), pg_size_pretty(sum(f.file_size)) from files_image f
+    -- select count(*), pg_size_pretty(sum(f.file_size)) from prod.files_image f
     -- where f.deployment_id in (select deployment_id from selected_deployments);
     select f.file_id, object_name from prod.files_image f
     join prod.mm_files_image_storage mfs on f.file_id = mfs.file_id
@@ -34,14 +34,14 @@ batches = [
     -- count 459566, total size 2406 GB
     with selected_deployments as (
         select d.deployment_id as deployment_id
-        from deployments d
-        left join mm_tags_deployments mm on mm.deployments_deployment_id = d.deployment_id
-        left join tags t on t.tag_id = mm.tags_tag_id
-        left join nodes n on d.node_id = n.node_id
+        from prod.deployments d
+        left join prod.mm_tags_deployments mm on mm.deployments_deployment_id = d.deployment_id
+        left join prod.tags t on t.tag_id = mm.tags_tag_id
+        left join prod.nodes n on d.node_id = n.node_id
         where t.name = 'FS1'
             and n.type = 'Pollinator Cam'
     )
-    -- select count(*), pg_size_pretty(sum(f.file_size)) from files_image f
+    -- select count(*), pg_size_pretty(sum(f.file_size)) from prod.files_image f
     -- where f.deployment_id in (select deployment_id from selected_deployments);
     select f.file_id, object_name from prod.files_image f
     join prod.mm_files_image_storage mfs on f.file_id = mfs.file_id
