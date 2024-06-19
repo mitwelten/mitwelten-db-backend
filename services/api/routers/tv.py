59b4cac4fe2e5b355bec6cb071912165b69c8a64
Cedric Spindler <cedric.spindler@fhnw.ch>
2024-06-19 18:19:51 +0200

    Implement WildCam TV interval algorithm in python instead of sql

--- services/api/routers/tv.py
+++ services/api/routers/tv.py
@@ -1,75 +1,41 @@
-from datetime import timedelta
-
 from api.database import database
 from api.dependencies import to_inclusive_range
-from api.models import TimeStampRange, TVStackSelectionRequest
-from api.tables import files_image
+from api.models import TVStackSelectionRequest
 
 from fastapi import APIRouter
-from sqlalchemy.sql import select, text
+from sqlalchemy.sql import text
 
 import credentials as crd
 
-
-router = APIRouter(tags=['images', 'walk'])
-
-async def post_stack_selection(body: dict):
-    print(body)
-    images = select(files_image).\
-        where(files_image.c.deployment_id == 824).\
-        order_by(files_image.c.time).limit(1000).offset(0);
-        # where(files_image.c.file_size < 1000000).\
-    return await database.fetch_all(images)
+router = APIRouter(tags=['images', 'wildcam-tv'])
 
 @router.post('/tv/stack-selection/')
-async def post_imagestack(body: TVStackSelectionRequest):
+async def post_stack_selection(body: TVStackSelectionRequest):
+    query = text(f'''
+    select * from {crd.db.schema}.files_image
+    where
+        deployment_id = :deployment_id
+        and :period ::tstzrange @> time
+    order by time
+    ''').bindparams(
+        deployment_id=body.deployment_id,
+        period=to_inclusive_range(body.period)
+    )
     if not body.interval:
-        query = text(f'''
-        select * from {crd.db.schema}.files_image
-        where
-            deployment_id = :deployment_id
-            and :period ::tstzrange @> time
-        order by time
-        ''').bindparams(
-            deployment_id=body.deployment_id,
-            period=to_inclusive_range(body.period)
-        )
+        return await database.fetch_all(query)
     else:
-        query = text(f'''
-        with
-            time_delta as (
-                select
-                    object_name,
-                    time,
-                    coalesce(time - lag(time) over (order by time), '0 seconds') as delta
-                from {crd.db.schema}.files_image
-                where
-                    deployment_id = :deployment_id
-                    and :period ::tstzrange @> time
-            ),
-            sum_group as (
-                select
-                    *,
-                    sum(case when dsum + delta > :interval then 1 else 0 end) over (order by time) as grp
-                from (
-                    select
-                        *,
-                        sum(delta) over (order by time) as dsum
-                    from time_delta
-                ) as delta_sums
-            ),
-            running_sum as (
-                select
-                    *,
-                    sum(delta) over (partition by grp order by time) as rsum
-                from sum_group
-            )
-        select *
-        from running_sum
-        where rsum > :interval
-        ''').bindparams(
-            interval=timedelta(seconds=body.interval),
-            deployment_id=body.deployment_id,
-            period=body.period
-        )
-    return await database.fetch_all(query)
+        records = await database.fetch_all(query)
+        interval = max(body.interval, 1)
+        filtered_records = []
+        delta_sum = 0
+        for i, r in enumerate(records):
+            if i == 0:
+                filtered_records.append(r)
+                continue
+            prev_time = records[i-1]['time']
+            curr_time = r['time']
+            delta_sum += (curr_time - prev_time).total_seconds()
+            if delta_sum >= interval:
+                filtered_records.append(r)
+                delta_sum = 0
+        return filtered_records
